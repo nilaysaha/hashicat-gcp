020514d8229614cf0ed1655df091189c3dbcfb8d
Nilay saha <saha.nilay@gmail.com>
2021-11-05 18:37:57 +0000

    added vpc module

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+module "network" {
+  source  = "app.terraform.io/testprod1972/network/google"
+  version = "3.4.0"
+  # insert required variables here
+  project_id   = "var.project"
+  network_name = "tproj-network-1"
+
+  subnets = [
+   {
+     subnet_name   = "tproj-subnet-1"
+     subnet_ip     = "10.100.10.0/24"
+     subnet_region = var.region
+   }
+  ]
+}
